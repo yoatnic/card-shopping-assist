64c8d4e82412532f07ed5bb93fc26e39f8b49bca
yoatnic <yoatnic@me.com>
2014-04-27 14:34:43 +0900

    add comment

--- get.rb
+++ get.rb
@@ -1,9 +1,14 @@
+# HTML取得用のライブラリ
 require 'uri'
 require 'open-uri'
+# HTMLパーサ
 require 'nokogiri'
+# 文字コード変換
 require 'kconv'
 
-# URLからHTMLを取得
+# URLからHTMLのオブジェクトを取得
+# @param[string] url HTMLを取得する対象のURL
+# @return HTMLのオブジェクト
 def get_doc(url)
   charset = nil
   html = open(url) do |f|
@@ -14,57 +19,80 @@ def get_doc(url)
   doc = Nokogiri::HTML.parse(html, nil, charset)
 end
 
-#価格情報を取得
+# 価格情報へのURLを取得
+# @param doc HTMLのオブジェクト
+# @return[string] 価格情報へのURL
 def find_price_link( doc )
   doc.css('p[@class="f9"]').each {|e|
     link = e.css('a')
     
     if (link && link.text.encode("UTF-8") == '【省略された価格情報を表示】')
-        return link.attribute('href').to_s
+      return link.attribute('href').to_s
     end
   }
   
   return nil
 end
 
+# 検索結果からカード名が完全一致するカードページのURLを取得
+# @param[string] str  カード名
+# @param doc HTMLのオブジェクト
+# @return カードページのURL
 def find_card_info(str, doc)
   doc.xpath('//td[@class="l w"]').each{|e|
     if e.text.to_s.encode("UTF-8") == str
       link = e.css('a')
-      dst = nil
-      if (link)
-        dst = get_doc('http://ocg.xpg.jp'+link.attribute('href').to_s)
-      end
+      dst = get_doc('http://ocg.xpg.jp'+link.attribute('href').to_s)
       
       return dst
     end
   }
+  return nil
 end
 
+# カードを扱うショップ、価格、レアリティを取得
+# ToDo: もうちょっとコードを綺麗にしたい
+# @param[string] str カード名
+# @return[hash] 
+# {
+#  'card-name' : カード名,
+#  'price-data'[array] : 価格データ
+#  [
+#   {'rarerity' : レアリティ, 'shop-name' ： ショップ名, 'price' : 価格}
+#  ]
+# }
 def get_card_price_info(str)
   cond = URI.escape(str.encode("Shift_JIS"))
   url = 'http://ocg.xpg.jp/search/search.fcgi?Name=' + cond + '&Mode=0&Code=%82%A0'
 
+  # カード検索を実施
   doc = get_doc(url)
 
+  if (!doc) return nil end
+
+  # カード名が複数ヒットした場合は、その中からカード名が完全一致するものを見つける
   if doc.xpath('//h1')[0].text.encode("Shift_JIS").include?( '検索結果'.encode("Shift_JIS", "UTF-8") )
     doc = find_card_info str, doc
   end
   
+  if (!doc) return nil end
+  
+  # カード名の取得
   card_name = doc.xpath('//h1')[0].text.encode("Shift_JIS")
 
+  # カードの価格情報のURLを取得
   price_info_url = find_price_link(doc);
   
-  if (!price_info_url)
-    return nil
-  end
+  if (!price_info_url) return nil end
   
   price_info_url = 'http://ocg.xpg.jp' + find_price_link(doc)
   
+  # 価格情報のページを取得
   price_doc = get_doc(price_info_url)
 
   target = nil
 
+  # 価格情報ページから価格が掲載されている<table>を取得
   price_doc.xpath('//table[@class="jHover f9"]').each {|e|
     if ( e.to_s.include?('情報取得日'.encode("Shift_JIS", "UTF-8")) ) then
       target = e
@@ -72,6 +100,7 @@ def get_card_price_info(str)
     end
   }
 
+  # カードの情報、各ショップの価格情報を抽出
   dst = {'card-name' => card_name.encode("UTF-8", "Shift_JIS"), 'price-data' => []}
 
   if (!target.nil?) then
@@ -90,6 +119,8 @@ def get_card_price_info(str)
   return dst
 end
 
+# 価格情報を整形して表示
+# @param[hash] data 表示するカードデータ
 def disp_card_name( data )
   data.each{|card|
     puts '<div>'
